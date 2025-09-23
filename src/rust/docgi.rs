0fa76f539a8cfb7c84cbb2a78d5da6c49d8b8e05
D R <metricstream@gmail.com>
2025-09-22 20:11:48 -0700

    added more common extensions (this part should be easy configurable)

--- src/rust/docgi.rs
+++ src/rust/docgi.rs
@@ -639,7 +639,7 @@ fn inner_main() -> Result<(), Box<dyn std::error::Error>> {
                 let dir = config.to_real_path(&config.get_project_home(&params.param("session")).unwrap_or(String::new()), None);
                 let dir_len = (&dir).len();
                 eprintln! {"Search for {string} in {dir:?}"}
-                let exts = ".java.rs.txt.md.cpp.pas.js.html.css.7b.rb.xml.kt";
+                let exts = ".java.rs.txt.md.cpp.pas.js.html.css.7b.rb.xml.kt.py.ts";
             
                 let files = web::list_files(&dir, &exts); // faster to pass an array of exts
                 //eprintln! {"...in {} files", files.len()}

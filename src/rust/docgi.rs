1e8821eaf589306a0c7e0b024bbd5f8994f1be9d
D R <metricstream@gmail.com>
2025-10-08 21:35:01 -0700

    added Swift support

--- src/rust/docgi.rs
+++ src/rust/docgi.rs
@@ -640,7 +640,7 @@ fn inner_main() -> Result<(), Box<dyn std::error::Error>> {
                 let dir = config.to_real_path(&config.get_project_home(&params.param("session")).unwrap_or_else(String::new), None);
                 let dir_len = (&dir).len();
                 eprintln! {"Search for {string} in {dir:?}"}
-                let exts = ".java.rs.txt.md.cpp.pas.js.html.css.7b.rb.xml.kt.py.ts";
+                let exts = ".java.rs.txt.md.cpp.pas.js.html.css.7b.rb.xml.kt.py.ts.swift";
             
                 let files = web::list_files(&dir, &exts); // faster to pass an array of exts
                 //eprintln! {"...in {} files", files.len()}

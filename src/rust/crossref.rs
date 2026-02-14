3c5555528741290d86e2aa89c0e3f6d9367cad23
Vernisaz <javaarchitect@msn.com>
2026-02-14 12:04:20 -0800

    bumped build number

--- src/rust/crossref.rs
+++ src/rust/crossref.rs
@@ -357,7 +357,7 @@ pub fn scan(reader: &mut Reader) -> Vec< Reference> {
                 if state == ExpComment {
                     state = prev_state.pop().unwrap()
                 }
-                eprintln!{"state befor comma {state:?} at {}:{}", reader.line, reader.line_offset}
+               // eprintln!{"state befor comma {state:?} at {}:{}", reader.line, reader.line_offset}
                 match state {
                     InDataDef | Start | Direct | ExpDirect => (),
                     InCallName => name.clear(),

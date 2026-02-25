4caa3da2d8087ae12ccdf29c114a937189f79f5b
Vernisaz <javaarchitect@msn.com>
2026-02-24 16:37:58 -0800

    removed unimplemented part and given recommendation using simweb instead

--- crate/web_cgi/param.rs
+++ crate/web_cgi/param.rs
@@ -1,6 +1,5 @@
-use std::{collections::HashMap,
-io, time::SystemTime};
 use simtime::get_datetime;
+use std::{collections::HashMap, env, io, time::SystemTime};
 
 #[derive(Debug)]
 pub struct Param {
@@ -8,11 +7,10 @@ pub struct Param {
     cookies: HashMap<String, String>,
 }
 
-pub const HTTP_DAYS_OF_WEEK: &[&str] = &[
-"Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed",];
+pub const HTTP_DAYS_OF_WEEK: &[&str] = &["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
 
 pub const HTTP_MONTH: &[&str] = &[
-"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec", 
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
 ];
 
 impl Default for Param {
@@ -27,54 +25,44 @@ impl Param {
             params: HashMap::new(),
             cookies: HashMap::new(),
         };
-        if let std::result::Result::Ok(query) = std::env::var(String::from("QUERY_STRING")) {
+        if let Ok(query) = env::var(String::from("QUERY_STRING")) {
             let parts = query.split("&");
             for part in parts {
                 if let Some(keyval) = part.split_once("=") {
-                    res.params.insert(
-                        res.url_comp_decode(keyval.0),
-                        res.url_comp_decode(keyval.1)
-                    );
+                    res.params
+                        .insert(res.url_comp_decode(keyval.0), res.url_comp_decode(keyval.1));
                 }
             }
         }
-        if let std::result::Result::Ok(header_cookies) = std::env::var(String::from("HTTP_COOKIE")) {
+        if let Ok(header_cookies) = env::var(String::from("HTTP_COOKIE")) {
             let parts = header_cookies.split(";");
             for part in parts {
                 if let Some(keyval) = part.split_once('=') {
-                    res.cookies.insert(
-                        keyval.0.trim().to_string(),
-                        keyval.1.to_string(),
-                    );
+                    res.cookies
+                        .insert(keyval.0.trim().to_string(), keyval.1.to_string());
                 }
             }
         }
         //
-        if let std::result::Result::Ok(method) = std::env::var(String::from("REQUEST_METHOD"))
-            && method == "POST" 
+        if let Ok(method) = env::var(String::from("REQUEST_METHOD"))
+            && method == "POST"
         {
             let mut user_input = String::new();
             let stdin = io::stdin();
-            if let Ok(content_type) = std::env::var(String::from("CONTENT_TYPE")) {
-                match  content_type.as_str() {
-                    "application/x-www-form-urlencoded" => {
-                        if let Ok(_ok) = stdin.read_line(&mut user_input) {
-                            let parts = user_input.split("&");
-                            for part in parts {
-                                if let Some(keyval) = part.split_once('=') {
-                                    res.params.insert(
-                                        res.url_comp_decode(keyval.0),
-                                        res.url_comp_decode(keyval.1),
-                                    );
-                                }
-                            }
+            if let Ok(content_type) = env::var(String::from("CONTENT_TYPE"))
+                && content_type == "application/x-www-form-urlencoded"
+            {
+                // use simweb to process other types
+                if let Ok(_ok) = stdin.read_line(&mut user_input) {
+                    let parts = user_input.split("&");
+                    for part in parts {
+                        if let Some(keyval) = part.split_once('=') {
+                            res.params.insert(
+                                res.url_comp_decode(keyval.0),
+                                res.url_comp_decode(keyval.1),
+                            );
                         }
-                        // sink reminded if any
                     }
-                    "multipart/form-data" => {
-                        // sink reminded if any
-                    }
-                    _ => () // sink reminded if any
                 }
             }
         }
@@ -85,16 +73,16 @@ impl Param {
     pub fn param(&self, key: impl AsRef<str>) -> Option<String> {
         self.params.get(key.as_ref()).cloned() // probably better to return as Option<&String> without using clone
     }
-    
+
     pub fn cookie(&self, key: impl AsRef<str>) -> Option<String> {
         self.cookies.get(key.as_ref()).cloned() // probably better to return as Option<&String> without using clone
     }
 
     pub fn path_info(&self) -> String {
-        if let Result::Ok(pi) = std::env::var(String::from("PATH_INFO")) {
+        if let Ok(pi) = env::var(String::from("PATH_INFO")) {
             pi.to_string()
         } else {
-        // since path info is never an empty string
+            // since path info is never an empty string
             "".to_string()
         }
     }
@@ -125,16 +113,21 @@ impl Param {
 pub fn http_format_time(time: SystemTime) -> String {
     let dur = time.duration_since(SystemTime::UNIX_EPOCH).unwrap();
     let (y, m, d, h, min, s, w) = get_datetime(1970, dur.as_secs());
-    format!("{}, {d:0>2} {} {y:0>2} {h:0>2}:{min:0>2}:{s:0>2} GMT",
-         HTTP_DAYS_OF_WEEK[w as usize], HTTP_MONTH[(m-1) as usize])
+    format!(
+        "{}, {d:0>2} {} {y:0>2} {h:0>2}:{min:0>2}:{s:0>2} GMT",
+        HTTP_DAYS_OF_WEEK[w as usize],
+        HTTP_MONTH[(m - 1) as usize]
+    )
 }
 
-pub fn to_web_separator(mut path: String ) -> String {
+pub fn to_web_separator(mut path: String) -> String {
     unsafe {
-        let path_vec: &mut [u8]= path.as_bytes_mut();
-    
+        let path_vec: &mut [u8] = path.as_bytes_mut();
+
         for c in path_vec {
-            if *c == b'\\' { *c = b'/';}
+            if *c == b'\\' {
+                *c = b'/';
+            }
         }
     }
     path

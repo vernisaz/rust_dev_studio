51c7f7e5ef0e1b82e252ae03b2e3bf2057b44529
Vernisaz <javaarchitect@msn.com>
2026-03-30 10:27:02 -0700

    improved error handling

--- tools/nonutf8/read_nonutf8.rs
+++ tools/nonutf8/read_nonutf8.rs
@@ -1,30 +1,31 @@
-extern crate simcolor;
 extern crate simcli;
-use simcolor::Colorized;
-use std::fs::File;
-use std::io::Read;
-use std::fs;
-use std::path::PathBuf;
-use std::io::Error;
+extern crate simcolor;
 use crate::simcli::CLI;
+use simcolor::Colorized;
+use std::fs::{self, File};
+use std::io::{Error, Read};
+use std::path::PathBuf;
 #[allow(unused)]
 const VERSION: &str = env!("VERSION");
 fn main() -> std::io::Result<()> {
     let mut cli = CLI::new();
     cli.description("Usage: nonutf8 <file name>");
     if cli.args().is_empty() {
-        return Err(Error::other(cli.get_description().unwrap().bright().blue()))
+        return Err(Error::other(cli.get_description().unwrap().bright().blue()));
     }
     let path = &cli.args()[0];
     let mut file = File::open(path)?;
-    let mut buf = vec![]; // with_capacity(file_len)
+    let mut buf = Vec::with_capacity(
+        file.metadata()?
+            .len()
+            .try_into()
+            .map_err(|_e| Error::other("not enough memory"))?,
+    );
     file.read_to_end(&mut buf)?;
     let contents = String::from_utf8_lossy(&buf);
     //print!("{contents}");
     let mut bak_path = PathBuf::from(path);
     bak_path.set_extension("bak");
-    if std::fs::rename(path, bak_path).is_ok() {
-        fs::write(path, contents.as_bytes())?;
-    }
-    Ok(())
+    fs::rename(path, bak_path)?;
+    fs::write(path, contents.as_bytes())
 }

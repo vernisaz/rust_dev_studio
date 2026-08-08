b04a88603c1cb6b65c0386b5d29dd9d34876f0d1
Vernisaz <javaarchitect@msn.com>
2026-08-07 20:05:59 -0700

    multiple files support, for example : nonutf src/*.rs will work

--- tools/nonutf8/read_nonutf8.rs
+++ tools/nonutf8/read_nonutf8.rs
@@ -7,25 +7,27 @@ use std::io::{Error, Read};
 use std::path::PathBuf;
 #[allow(unused)]
 const VERSION: &str = env!("VERSION");
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut cli = CLI::new();
-    cli.description("Usage: nonutf8 <file name>");
+    cli.description("Usage: nonutf8 <file name>...");
     if cli.args().is_empty() {
-        return Err(Error::other(cli.get_description().unwrap().bright().blue()));
+        return Err(Box::new(cli.get_description().unwrap().bright().blue()));
     }
-    let path = &cli.args()[0];
-    let mut file = File::open(path)?;
-    let mut buf = Vec::with_capacity(
-        file.metadata()?
-            .len()
-            .try_into()
-            .map_err(|_e| Error::other("not enough memory"))?,
-    );
-    file.read_to_end(&mut buf)?;
-    let contents = String::from_utf8_lossy(&buf);
-    //print!("{contents}");
-    let mut bak_path = PathBuf::from(path);
-    bak_path.set_extension("bak");
-    fs::rename(path, bak_path)?;
-    fs::write(path, contents.as_bytes())
+    for path in cli.args() {
+        let mut file = File::open(path)?;
+        let mut buf = Vec::with_capacity(
+            file.metadata()?
+                .len()
+                .try_into()
+                .map_err(|_e| Error::other("not enough memory"))?,
+        );
+        file.read_to_end(&mut buf)?;
+        let contents = String::from_utf8_lossy(&buf);
+        //print!("{contents}");
+        let mut bak_path = PathBuf::from(path);
+        bak_path.set_extension("bak");
+        fs::rename(path, bak_path)?;
+        fs::write(path, contents.as_bytes())?;
+    }
+    Ok(())
 }
